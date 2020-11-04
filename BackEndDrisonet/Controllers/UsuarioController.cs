7469d18377d97131fdd3fe70fda767366e416e9d
juanchos2018 <jcarlossenati@gmail.com>
2020-11-04 02:22:13 -0500

    ConsultaRuc

--- BackEndDrisonet/Controllers/UsuarioController.cs
+++ BackEndDrisonet/Controllers/UsuarioController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Entidad;
 using Microsoft.AspNetCore.Mvc;
 using Negocio;
+using Newtonsoft.Json;
 
 namespace BackEndDrisonet.Controllers
 {
@@ -25,6 +27,23 @@ namespace BackEndDrisonet.Controllers
                 completado = true;
             }
             return Json(completado);
+        }
+
+        [Route("Usuario/ConsultaRuc")]
+        public async Task<ActionResult> Get_Consulta_Ruc([FromBody] Empresa o)
+        {
+            HttpClient client = new HttpClient();
+            String URL2 = "https://quertium.com/api/v1/sunat/ruc/" + o.ruc + "?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.MTM3Mw.x-jUgUBcJukD5qZgqvBGbQVMxJFUAIDroZEm4Y9uTyg";
+            HttpResponseMessage response = await client.GetAsync(URL2);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var empresa = JsonConvert.DeserializeObject<Empresa>(data);
+                return Json(empresa);
+            }
+
+            return Json("sin Datos");
+
         }
     }
 }
---
+++ Entidad/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidad
+{
+   public class Empresa
+    {
+        public string ruc { get; set; }
+        public string nombre { get; set; }
+        public string nombre_comercial { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+
+    }
+}
